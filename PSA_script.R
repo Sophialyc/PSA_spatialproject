b88396575793938900e85aecf91579693304a0c0
Sophia Cheung <ucfaheu@ucl.ac.uk>
2023-01-03 13:16:51 +0000

    kriging model attempted

--- PSA_script.R
+++ PSA_script.R
@@ -88,11 +88,7 @@ Africa_STHpercentage <-merge(Africa_proj_sim, STH_percent, by='country', all=T)
 
 #sort the country according to the STH percentage values
 Africa_STH_D_order <- Africa_STHpercentage[order(-Africa_STHpercentage$percent),]
-
-Africa_STH_D_order %>% 
-  top_n(10, percent) %>% 
-  ggplot() + aes(country, percent) + geom_point() 
-
+Africa_STH_D_order
 # Ignore the warning message, the empty units are purposely added to allow the legend to show that some countries have missing data
 Africa_STH_D_order <- Africa_STHpercentage[order(-Africa_STHpercentage$percent),]
 #map the STH prevalence in Africa based on the data collected
@@ -103,12 +99,12 @@ tm_shape(Africa_proj_sim) +
   tm_shape(Africa_STH_D_order) +
   tm_fill(col= 'percent') +
   tm_style("white") +
-  tm_layout( "STH Prevalence across Africa (%)", title.size = 1, title.position= c('center','bottom')) +
-  tm_scale_bar(position = c("left", "bottom"), width = 0.15) +
+  tm_layout( "STH Prevalence across Africa (%)", title.size = 0.7, title.position= c('center','bottom')) +
+  tm_scale_bar(position = c("left", "bottom"), width = 0.15, size = 0.5) +
   tm_compass(position = c("left", "top"), size = 2) 
 
 
-#THis map only shows the overview of the Whole African continent using mean values, it does not show accurate results of each country
+#This map only shows the overview of the Whole African continent using mean values, it does not show accurate results of each country
 #Before moving on to focus on Nigeria, the following analysis will use the health district for performing geostatistical modelling
 st_crs(Health_district)$proj4string
 Health_district <- Health_district %>%
@@ -118,7 +114,6 @@ Health_district_proj <- Health_district %>%
   st_transform(., 3857)
 
 summary(Health_district_proj)
-
 
 # filter out only Southern Africa's health district boundaries
 Health_district_proj <- Health_district_proj%>%
@@ -149,8 +144,8 @@ NGA_STH_sub <- NGA_STH_pts[NGA_healthdistrict, ]
 #inspect
 tm_shape(NGA_healthdistrict) + tm_polygons(alpha = 0, border.col = "black") + 
   tm_shape(NGA_STH_sub) + tm_dots(col = 'blue') + 
-  tm_scale_bar(position = c("left","bottom")) +
-  tm_compass(position = c("right", "bottom"))
+  tm_scale_bar(position = c("right","bottom")) +
+  tm_compass(position = c("left", "top"))
 
 NGA_STH_joined <- st_join(NGA_healthdistrict, NGA_STH_sub, by = 'iu_id')
 
@@ -161,12 +156,14 @@ NGA_STH_sub <- NGA_STH_sub%>%
   mutate(percentage = sth_positive/sth_examined)%>%
   mutate(perc_to_100 = percentage*100)
 
+
 #since the points are not where STH is detected, instead they are points where STH has been examined and diagnosed
 #try kriging on NGA
 library("gstat")
 library("geoR")
+library(sp)
 
-#inspect
+ #inspect
 tm_shape(NGA_healthdistrict) + tm_polygons(alpha = 0, border.col = "black") + 
   tm_shape(NGA_STH_sub) + tm_dots() + 
   tm_scale_bar(position = c("left","bottom")) +
@@ -182,12 +179,46 @@ tm_shape(NGA_STH_joined) +
 
 NGA_STH_sub_sp <- as(NGA_STH_sub, "Spatial")
 
-NGASTH_emp.variogram <- variogram(percentage~1,NGA_STH_sub_sp)
+NGASTH_emp.variogram <- variogram(percentage~1, NGA_STH_sub_sp)
 NGASTH_emp.variogram
 plot(NGASTH_emp.variogram)
-#nugget is around 380, range:200000 , partial sill: 2170
+#nugget - 0.024
+#range - 200005 
+#sill - 0.033
+#partial sill - 0.018 
 # Fit best model
-best_SASTH_emp.variogram <- fit.variogram(NGASTH_emp.variogram, model = vgm(c("Exp", "Gau", "Sph")))
-best_SASTH_emp.variogram
+best_NGASTH_emp.variogram <- fit.variogram(NGASTH_emp.variogram, model = vgm(c("Exp", "Gau", "Sph")))
+best_NGASTH_emp.variogram
+plot(NGASTH_emp.variogram, best_NGASTH_emp.variogram, main  = "Gaussian model (Nug: 0.023445432, PSill:0.008277129, Range: 161882.2m)")
+
+#build blank raster template
+RasterTemplate <- raster(NGA_STH_sub_sp)
+res(RasterTemplate) <- 5000
+grid.interpolation <- as(RasterTemplate, 'SpatialGrid')
+# interpolation 
+modelKrigingGAU <- gstat(formula = percentage~1, locations = NGA_STH_sub_sp, model = best_NGASTH_emp.variogram)
+# add results of the interpolation to the raster template --> predicted STH and variation of STH
+Kriged_STH <- predict(modelKrigingGAU, grid.interpolation)
+
+# save both prediction and variance to a brick raster
+brickedKriged_STH_Results <- brick(Kriged_STH)
+# slice the layers out
+exp.prediction <- raster(brickedKriged_STH_Results, layer = 1)
+exp.variance <- raster(brickedKriged_STH_Results, layer = 2)
+
+writeRaster(exp.prediction, "Predicted STH levels in NGA.tif", format="GTiff", overwrite = TRUE)
+writeRaster(exp.variance, "Variance STH levels in NGA.tif", format="GTiff", overwrite = TRUE)
+
+NGA_shp_sp <- as(NGA_STH_joined, "Spatial")
+exp.prediction_masked <- mask(exp.prediction, NGA_shp_sp)
 
 
+frameExtent <- st_bbox(NGA_shp_sp)
+frameExtent
+
+tm_shape(exp.prediction_masked, bbox = frameExtent) + tm_raster(title = "Predicted STH ppb", style = "cont", palette = "Reds") +
+  tm_shape(NGA_shp_sp) + tm_polygons(alpha = 0, border.col = "black") +
+  tm_scale_bar(position = c("left","bottom")) 
+  tm_shape(NGA_STH_joined) + tm_dots() +
+  tm_compass(position = c("right", "bottom")) +
+  tm_layout(frame = FALSE, legend.title.size = 0.5, legend.text.size = 0.5, legend.position = c("right", "bottom"))
